fe1d5363a65a91ac68b3ef247d1d66732e1ce7b9
BelenOrnad <156037270+BelenOrnad@users.noreply.github.com>
2024-02-14 18:07:27 -0300

    Add files via upload

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -1,17 +1,40 @@
 //Es un e-commerce de ropa e indumentaria
+let productos = ["remeras", "vestidos", "shorts", "campera", "aros"]
 
 let nombre = prompt ("Ingresar un nombre");
 
 console.log ("Hola" , nombre, ", que tipo de ropa le gustaria comprar?");
+console.log (productos[0])
+console.log (productos[1])
+console.log (productos[2])
+console.log (productos[3])
+console.log (productos[4])
 
 let ropa = prompt ("Ingresar algun articulo");
 
-if (ropa == "remeras"){
-    console.log ("Remera blanca $7.000");
-    console.log ("Remera negra $5.800");
-    console.log ("Remera azul $4.000");
+if (ropa == productos[0]){
+    let rem = [
+        {
+            articulo: "remera",
+            color: "blanco",
+            precio: 7000
+        },
+        {
+            articulo: "remera",
+            color: "negro",
+            precio: 5800
+        },
+        {
+            articulo: "remera",
+            color: "azul",
+            precio: 4000
+        }
+    ]
+    console.log (rem [0]);
+    console.log (rem [1]);
+    console.log (rem [2]);
 
-    let opUno = prompt ("Elegí una opcion")
+    let opUno = prompt ("Elegí una opcion (introducir articulo y color)")
     if (opUno = "remera blanca" ){
         let opcionUno = 7000;
 
@@ -69,12 +92,29 @@ if (pago == "tarjeta"){
     }
     
 
-}else if (ropa == "shorts"){
-    console.log ("Short Jean $6.700")
-    console.log ("Short negro $10.000")
-    console.log ("Short militar $7.000")
+}else if (ropa == productos[1]){
+    let short = [
+        {
+            articulo: "short",
+            color: "jean",
+            precio: 6700
+        },
+        {
+            articulo: "short",
+            color: "negro",
+            precio: 10000
+        },
+        {
+            articulo: "short",
+            color: "militar",
+            precio: 7000
+        }
+    ]
+    console.log (short [0]);
+    console.log (short [1]);
+    console.log (short [2]);
     
-    let opUno = prompt ("Elegí una opcion")
+    let opUno = prompt ("Elegí una opcion (introducir articulo y color)")
     if (opUno = "short jean" ){
         let opcionUno = 6700;
 
@@ -131,12 +171,29 @@ if (pago == "tarjeta"){
         alert("opcion no disponible")
     }
     
-}else if (ropa == "vestido"){
-    console.log ("Vestido floreado $2.500")
-    console.log ("Vestido celeste $4.400")
-    console.log ("Vestido rosa $9.000")
+}else if (ropa == productos[2]){
+    let vest = [
+        {
+            articulo: "vestido",
+            color: "floreado",
+            precio: 2500
+        },
+        {
+            articulo: "vestido",
+            color: "celeste",
+            precio: 44000
+        },
+        {
+            articulo: "vestido",
+            color: "rosa",
+            precio: 9000
+        }
+    ]
+    console.log (vest [0]);
+    console.log (vest [1]);
+    console.log (vest[2]); 
 
-    let opUno = prompt ("Elegí una opcion")
+    let opUno = prompt ("Elegí una opcion (introducir articulo y color)")
     if (opUno = "vestido floreado" ){
         let opcionUno = 2500;
 
@@ -193,6 +250,164 @@ if (pago == "tarjeta"){
         alert("opcion no disponible")
     }
 
+
+}else if (ropa == productos[3]){
+    let camp = [
+        {
+            articulo: "campera",
+            color: "negra",
+            precio: 9700
+        },
+        {
+            articulo: "campera",
+            color: "azul",
+            precio: 7400
+        },
+        {
+            articulo: "campera",
+            color: "gris",
+            precio: 9500
+        }
+    ]
+    console.log (camp [0]);
+    console.log (camp [1]);
+    console.log (camp[2]); 
+
+    let opUno = prompt ("Elegí una opcion (introducir articulo y color)")
+    if (opUno = "campera negra" ){
+        let opcionUno = 9700;
+
+        let cantUno = prompt ("Cuantos?")
+    parseFloat (cantUno)
+
+    let tot = (opcionUno * cantUno)
+
+    let pago = prompt ("Va a pagar con efectivo o tarjeta?");
+
+if (pago == "tarjeta"){
+    console.log ("Tiene un 10% de recargo")
+    console.log ("Su total a pagar es $", tot + (tot * (10 / 100)))
+}else{
+    console.log ("No tiene ningun recargo")
+    console.log ("Su total a pagar es $", tot)
+}
+
+    }else if (opUno = "campera azul" ){
+        const opcionUno = 7400;
+        let cantUno = prompt ("Cuantos?")
+    parseFloat (cantUno)
+
+    let tot = (opcionUno * cantUno)
+
+    let pago = prompt ("Va a pagar con efectivo o tarjeta?");
+
+if (pago == "tarjeta"){
+    console.log ("Tiene un 10% de recargo")
+    console.log ("Su total a pagar es $", tot + (tot * (10 / 100)))
+}else{
+    console.log ("No tiene ningun recargo")
+    console.log ("Su total a pagar es $", tot)
+}
+
+    }else  if (opUno = "campera gris" ){
+        let opcionUno = 9500;
+        let cantUno = prompt ("Cuantos?")
+    parseFloat (cantUno)
+
+    let tot = (opcionUno * cantUno)
+
+    let pago = prompt ("Va a pagar con efectivo o tarjeta?");
+
+if (pago == "tarjeta"){
+    console.log ("Tiene un 10% de recargo")
+    console.log ("Su total a pagar es $", tot + (tot * (10 / 100)))
+}else{
+    console.log ("No tiene ningun recargo")
+    console.log ("Su total a pagar es $", tot)
+}
+
+    }else{
+        alert("opcion no disponible")
+    }
+
+} else if (ropa == productos[4]){
+    let aro = [
+        {
+            articulo: "aros",
+            color: "plateados",
+            precio: 2700
+        },
+        {
+            articulo: "aros",
+            color: "dorados",
+            precio: 3200
+        },
+        {
+            articulo: "aros",
+            color: "rosas",
+            precio: 1500
+        }
+    ]
+    console.log (aro [0]);
+    console.log (aro [1]);
+    console.log (aro[2]); 
+
+    let opUno = prompt ("Elegí una opcion (introducir articulo y color)")
+    if (opUno = "aros plateados" ){
+        let opcionUno = 2700;
+
+        let cantUno = prompt ("Cuantos?")
+    parseFloat (cantUno)
+
+    let tot = (opcionUno * cantUno)
+
+    let pago = prompt ("Va a pagar con efectivo o tarjeta?");
+
+if (pago == "tarjeta"){
+    console.log ("Tiene un 10% de recargo")
+    console.log ("Su total a pagar es $", tot + (tot * (10 / 100)))
+}else{
+    console.log ("No tiene ningun recargo")
+    console.log ("Su total a pagar es $", tot)
+}
+
+    }else if (opUno = "aros dorados" ){
+        const opcionUno = 3200;
+        let cantUno = prompt ("Cuantos?")
+    parseFloat (cantUno)
+
+    let tot = (opcionUno * cantUno)
+
+    let pago = prompt ("Va a pagar con efectivo o tarjeta?");
+
+if (pago == "tarjeta"){
+    console.log ("Tiene un 10% de recargo")
+    console.log ("Su total a pagar es $", tot + (tot * (10 / 100)))
+}else{
+    console.log ("No tiene ningun recargo")
+    console.log ("Su total a pagar es $", tot)
+}
+
+    }else  if (opUno = "aros rosas" ){
+        let opcionUno = 1500;
+        let cantUno = prompt ("Cuantos?")
+    parseFloat (cantUno)
+
+    let tot = (opcionUno * cantUno)
+
+    let pago = prompt ("Va a pagar con efectivo o tarjeta?");
+
+if (pago == "tarjeta"){
+    console.log ("Tiene un 10% de recargo")
+    console.log ("Su total a pagar es $", tot + (tot * (10 / 100)))
+}else{
+    console.log ("No tiene ningun recargo")
+    console.log ("Su total a pagar es $", tot)
+}
+
+    }else{
+        alert("opcion no disponible")
+    }
 
 }else{
     console.log ("No hay stock")
